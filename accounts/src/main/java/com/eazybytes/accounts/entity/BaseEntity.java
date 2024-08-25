ff1750addedb9574408da014db317c2a6f4a1d5a
sharang <sharanggupta96@gmail.com>
2024-08-25 21:37:14 +0100

    Added JPA auditing

--- accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
+++ accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.eazybytes.accounts.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import lombok.Getter;
@@ -10,10 +11,12 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @ToString
