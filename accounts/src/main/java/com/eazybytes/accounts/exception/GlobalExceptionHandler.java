8e76ef80a6f72461394274cd0243051ee65556f7
sharang <sharanggupta96@gmail.com>
2024-08-25 17:58:12 +0100

    Added generic exception handler

--- accounts/src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
@@ -36,4 +36,16 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGenericException(Exception exception,
+                                                                            WebRequest webRequest) {
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
