7af0b4d8442694525b8c16dfa56070adbfffbeed
sharang <sharanggupta96@gmail.com>
2024-09-02 14:52:21 +0100

    Updated to include eureka server

---
+++ eurekaserver/src/test/java/dev/sharanggupta/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sharanggupta.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
