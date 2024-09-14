ef8fb626606f6793e40176632c90c5c3867481c5
sharang <sharanggupta96@gmail.com>
2024-09-14 15:17:57 +0100

    implemented the circuit breaker pattern for microservice failures

--- gatewayserver/src/main/java/dev/sharanggupta/gatewayserver/config/RoutingConfig.java
+++ gatewayserver/src/main/java/dev/sharanggupta/gatewayserver/config/RoutingConfig.java
@@ -14,18 +14,24 @@ public class RoutingConfig {
         return builder.routes()
                 .route("account_route", r -> r
                         .path("/eazybank/account/**")
-                        .filters(f->f.rewritePath("/eazybank/account/(?<segment>.*)", "/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .filters(f->f
+                                .rewritePath("/eazybank/account/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("account").setFallbackUri("forward:/contactSupport")))
                         .uri("lb://ACCOUNT"))
                 .route("card_route", r -> r
                         .path("/eazybank/card/**")
-                        .filters(f->f.rewritePath("/eazybank/card/(?<segment>.*)", "/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .filters(f->f
+                                .rewritePath("/eazybank/card/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("card").setFallbackUri("forward:/contactSupport")))
                         .uri("lb://CARD"))
                 .route("loan_route", r -> r
                         .path("/eazybank/loan/**")
-                        .filters(f->f.rewritePath("/eazybank/loan/(?<segment>.*)", "/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .filters(f->f
+                                .rewritePath("/eazybank/loan/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("loan").setFallbackUri("forward:/contactSupport")))
                         .uri("lb://LOAN"))
                 .build();
     }
---
+++ gatewayserver/src/main/java/dev/sharanggupta/gatewayserver/controller/FallbackController.java
@@ -0,0 +1,14 @@
+package dev.sharanggupta.gatewayserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @RequestMapping("/contactSupport")
+    public Mono<String> contactSupport() {
+        return Mono.just("An error occurred, Please try again later or contact the Support team!");
+    }
+}
